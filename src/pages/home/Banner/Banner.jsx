815122e514af9b3198d6041a788bca8b10f6d16f
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-10-25 20:46:54 +0600

    added reviewwe

--- src/pages/home/Banner/Banner.jsx
+++ src/pages/home/Banner/Banner.jsx
@@ -4,48 +4,68 @@ import img2 from "../../../assets/assets/watch5.png";
 import img3 from "../../../assets/assets/watch7.png";
 import img5 from "../../../assets/assets/watch3.png";
 import img6 from "../../../assets/assets/watch2.png";
+import AwesomeSlider from "react-awesome-slider";
+import "react-awesome-slider/dist/styles.css";
 
 const Banner = () => {
+  const bannars = [
+    {
+      id: 1,
+      title: " Plumbing Service",
+      subTitle1: "We Provide World Class",
+      subTitle2: "House shifting services in Dhaka",
+      descripttion:
+        "Welcome to PACK & SHIFT Removal group, Professional House Shifting Service to anywhere in Bangladesh from Dhaka City. AlsoInternational movers and packers Any country of the world",
+      image:
+        "https://i.ibb.co/SmK7wt7/29709195-rs27rwix6gc44dgjnwisuy5m-Extra-Large.webp",
+    },
+    {
+      id: 2,
+      title: "Expert Plumbing Service You Can Trust.",
+      subTitle1: "We Provide World Class",
+      subTitle2: "House shifting services in Dhaka",
+      descripttion:
+        "Welcome to PACK & SHIFT Removal group, Professional House Shifting Service to anywhere in Bangladesh from Dhaka City. AlsoInternational movers and packers Any country of the world",
+      image: "https://i.ibb.co/qprYHxR/s-l1200.webp",
+    },
+    {
+      id: 3,
+      title: "Expert Plumbing Service You Can Trust.",
+      subTitle1: "We Provide World Class",
+      subTitle2: "House shifting services in Dhaka",
+      descripttion:
+        "Welcome to PACK & SHIFT Removal group, Professional House Shifting Service to anywhere in Bangladesh from Dhaka City. AlsoInternational movers and packers Any country of the world",
+      image: "https://i.ibb.co/syrSgf5/84-37-731.webp",
+    },
+  ];
+
   return (
     <div>
-      <div className="carousel w-full">
-        <div id="item1" className="carousel-item w-full">
-          <img src={img5} className="w-full h-[700px]" />
-        </div>
-        <div id="item2" className="carousel-item w-full">
-          <img src={img2} className="w-full h-[700px]" />
-        </div>
-        <div id="item3" className="carousel-item w-full">
-          <img src={img3} className="w-full h-[700px]" />
-        </div>
-        <div id="item4" className="carousel-item w-full">
-          <img src={img6} className="w-full h-[700px]" />
-        </div>
-      </div>
-      <div className="flex justify-center w-full py-2 gap-2 ">
-        <a href="#item1" className="btn btn-md bg-slate-300 rounded-full ">
-          1
-        </a>
-        <a href="#item2" className="btn btn-md bg-slate-300 rounded-full">
-          2
-        </a>
-        <a href="#item3" className="btn btn-md bg-slate-300 rounded-full">
-          3
-        </a>
-        <a href="#item4" className="btn btn-md bg-slate-300 rounded-full">
-          4
-        </a>
-      </div>
-      <div className="text-lg bg-slate-100 rounded-2xl py-1">
-        <marquee behavior="" direction="">
-          Shoe is an item of footwear intended to protect and comfort the human
-          foot. Though the human foot can adapt to varied terrains and climate
-          conditions, it is vulnerable, and shoes provide protection. Form was
-          originally tied to function but over time shoes also became fashion
-          items. Some shoes are worn as safety equipment, such as steel-toe
-          boots, which are required footwear at industrial worksites.
-        </marquee>
-      </div>
+      {""}
+      <AwesomeSlider
+        animation="foldOutAnimation"
+        className="h-[270px] md:h-[70vh] lg:h-[80vh] w-[100vw]"
+      >
+        {bannars?.map((banner) => (
+          <>
+            <div
+              key={bannars?.id}
+              className="hero min-w-[100vw] min-h-[100vh] object-fill"
+              style={{
+                backgroundImage: `url(${banner?.image})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            >
+              <div className="hero-overlay bg-opacity-60"></div>
+              <div className="hero-content w-full text-center text-neutral-content">
+                <div className="max-w-md"></div>
+              </div>
+            </div>
+          </>
+        ))}
+      </AwesomeSlider>
     </div>
   );
 };
--- src/pages/home/Category/Category.jsx
+++ src/pages/home/Category/Category.jsx
@@ -1,11 +1,16 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
+import "../Category/Category.css";
+import {
+  Navigation,
+  Autoplay,
+  Keyboard,
+  Pagination,
+  Scrollbar,
+} from "swiper/modules";
 import "swiper/css";
-import "swiper/css/pagination";
 
-// import required modules
-import { Pagination } from "swiper/modules";
+import { FaQuoteLeft, FaQuoteRight, FaStar } from "react-icons/fa";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
 import img1 from "../../../assets/assets/category/watch1.png";
 import img2 from "../../../assets/assets/category/watch16.png";
@@ -19,72 +24,138 @@ import img8 from "../../../assets/assets/category/watch15.png";
 import SectionTitle from "../../../components/sectionTitle/SectionTitle";
 
 const Category = () => {
+  const SERVICE = [
+    {
+      id: 1,
+      title: " Plumbing Service",
+      subTitle1: "We Provide World Class",
+      subTitle2: "House shifting services in Dhaka",
+      descripttion:
+        "Welcome to PACK & SHIFT Removal group, Professional House Shifting Service to anywhere in Bangladesh from Dhaka City. AlsoInternational movers and packers Any country of the world",
+      image:
+        "https://i.ibb.co/SmK7wt7/29709195-rs27rwix6gc44dgjnwisuy5m-Extra-Large.webp",
+    },
+    {
+      id: 2,
+      title: "Expert Plumbing Service You Can Trust.",
+      subTitle1: "We Provide World Class",
+      subTitle2: "House shifting services in Dhaka",
+      descripttion:
+        "Welcome to PACK & SHIFT Removal group, Professional House Shifting Service to anywhere in Bangladesh from Dhaka City. AlsoInternational movers and packers Any country of the world",
+      image: "https://i.ibb.co/qprYHxR/s-l1200.webp",
+    },
+    {
+      id: 3,
+      title: "Expert Plumbing Service You Can Trust.",
+      subTitle1: "We Provide World Class",
+      subTitle2: "House shifting services in Dhaka",
+      descripttion:
+        "Welcome to PACK & SHIFT Removal group, Professional House Shifting Service to anywhere in Bangladesh from Dhaka City. AlsoInternational movers and packers Any country of the world",
+      image: "https://i.ibb.co/syrSgf5/84-37-731.webp",
+    },
+    {
+      id: 4,
+      title: "Expert Plumbing Service You Can Trust.",
+      subTitle1: "We Provide World Class",
+      subTitle2: "House shifting services in Dhaka",
+      descripttion:
+        "Welcome to PACK & SHIFT Removal group, Professional House Shifting Service to anywhere in Bangladesh from Dhaka City. AlsoInternational movers and packers Any country of the world",
+      image: "https://i.ibb.co/syrSgf5/84-37-731.webp",
+    },
+    {
+      id: 5,
+      title: "Expert Plumbing Service You Can Trust.",
+      subTitle1: "We Provide World Class",
+      subTitle2: "House shifting services in Dhaka",
+      descripttion:
+        "Welcome to PACK & SHIFT Removal group, Professional House Shifting Service to anywhere in Bangladesh from Dhaka City. AlsoInternational movers and packers Any country of the world",
+      image: "https://i.ibb.co/syrSgf5/84-37-731.webp",
+    },
+  ];
+
   return (
-    <div className="mt-10 bg-white">
-      <section className="bg-white">
-        <SectionTitle
-          subHeading={"From 9.00am to 11.00pm"}
-          heading={"Order online"}
-        />
-        <Swiper
-          slidesPerView={1}
-          spaceBetween={10}
-          pagination={{
-            clickable: true,
-          }}
-          breakpoints={{
-            "@0.00": {
-              slidesPerView: 1,
-              spaceBetween: 10,
-            },
-            "@0.75": {
-              slidesPerView: 2,
-              spaceBetween: 20,
-            },
-            "@1.00": {
-              slidesPerView: 3,
-              spaceBetween: 40,
-            },
-            "@1.50": {
-              slidesPerView: 4,
-              spaceBetween: 50,
-            },
-          }}
-          modules={[Pagination]}
-          className="mySwiper mb-16"
-        >
-          <SwiperSlide>
-            <img src={img1} alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            {" "}
-            <img src={img2} alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            {" "}
-            <img src={img3} alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            {" "}
-            <img src={img4} alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            {" "}
-            <img src={img5} alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            {" "}
-            <img src={img6} alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            {" "}
-            <img src={img7} alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            {" "}
-            <img src={img8} alt="" />
-          </SwiperSlide>
-        </Swiper>
+    <div className="bg-[#F5F8FE] w-full lg:w-[1440px] mx-auto">
+      <section className="container relative py-20">
+        <div className="flex flex-wrap">
+          <div className="w-full px-4">
+            <div className="mx-auto mb-12 max-w-[510px] text-center lg:mb-20">
+              <span className="block mb-2 text-lg font-semibold text-primary">
+                Read trusted reviews from our customers
+              </span>
+              <h2 className="mb-4 text-3xl font-bold text-dark sm:text-4xl md:text-[40px]">
+                What Peoples Say
+              </h2>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-10">
+          <Swiper
+            // install Swiper modules
+
+            modules={[Navigation, Autoplay, Keyboard, Pagination, Scrollbar]}
+            slidesPerView={1}
+            spaceBetween={20}
+            navigation={{
+              prevEl: ".prev",
+              nextEl: ".next",
+            }}
+            loop={true}
+            autoplay={{
+              delay: 5000,
+              disableOnInteraction: false,
+            }}
+            pagination={{ clickable: true }}
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              768: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+              },
+            }}
+            className="testimonal"
+          >
+            {SERVICE?.map((review, i) => (
+              <SwiperSlide className="pt-3" key={i}>
+                <div className="mb-10">
+                  <figure className="cursor-pointer">
+                    <img
+                      width={100}
+                      height={100}
+                      src={review?.image}
+                      alt=""
+                      className="w-[140px] lg:-mb-16 p-2 mx-auto object-cover h-[140px] rounded-full ring"
+                    />
+                  </figure>
+                  <blockquote className="blockquote bg-blue-100 flex h-full flex-col justify-center group rounded-2xl cursor-pointer transition-all duration-300  sm:pt-16 sm:p-8">
+                    <p className="text-2xl mb-1 text-center font-bold text-primaryColor sm:text-3xl">
+                      {review?.title}
+                    </p>
+
+                    {/* <div className="flex justify-center mt-2 gap-0.5 ">
+                      {Array(review?.title)
+                        .fill(0)
+                        .map((index, i) => (
+                          <FaStar key={i} className="w-6 h-6 text-yellow-500" />
+                        ))}
+                    </div> */}
+                  </blockquote>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div className="absolute right-20 bottom-10 hidden md:flex gap-5">
+          <button className="prev z-10 w-10 h-10 rounded-full bg-blue-500 shadow-xl flex items-center hover:-translate-x-1 transition-all duration-300 justify-center group">
+            <AiOutlineLeft className="w-6 h-6 text-white" />
+          </button>
+          <button className="next z-10 w-10 h-10 rounded-full bg-blue-500 shadow-xl flex items-center hover:translate-x-1 transition-all duration-300 justify-center group">
+            <AiOutlineRight className="w-6 h-6 text-white" />
+          </button>
+        </div>
       </section>
     </div>
   );

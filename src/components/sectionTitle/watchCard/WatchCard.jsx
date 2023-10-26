2e2a2889395c1013e1f4e3845718cc0631f5436c
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-10-26 14:34:29 +0600

    added more

--- src/components/sectionTitle/watchCard/WatchCard.jsx
+++ src/components/sectionTitle/watchCard/WatchCard.jsx
@@ -1,5 +1,50 @@
+import { useContext } from "react";
+import { AuthContext } from "../../../Providers/AuthProviders";
+import { getBaseUrl } from "../../../hooks/BaseURL";
+import Swal from "sweetalert2";
+import { useLocation, useNavigate } from "react-router-dom";
+
 const WatchCard = ({ item }) => {
-  const { image, price, name, details } = item;
+  const { image, price, name, details, _id } = item;
+  const router = useNavigate();
+
+  const location = useLocation();
+  const { user } = useContext(AuthContext);
+
+  const handleAddToCart = (item) => {
+    console.log(item);
+
+    if (user && user.email) {
+      const orderItem = { _id, name, image, price, details, email: user.email };
+      fetch("http://localhost:5000/api/v1/cart/create-cart", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(orderItem),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.data) {
+            Swal.fire("added to cart");
+          }
+        });
+    } else {
+      Swal.fire({
+        title: "Please Login First?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, Login",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          router("/login", { state: { from: location } });
+        }
+      });
+    }
+  };
+
   return (
     <div className="card  card-compact lg:w-96  shadow-xl hover:shadow-xl transition duration-300 ease-in-out transform hover:scale-105">
       <figure>
@@ -13,7 +58,10 @@ const WatchCard = ({ item }) => {
         <p>{details.slice(0, 100)}...</p>
 
         <div className="card-actions justify-center">
-          <button className="btn w-full rounded-3xl text-black  border-black hover:border-gray-900 px-4 py-2 ">
+          <button
+            onClick={() => handleAddToCart(item)}
+            className="btn w-full rounded-3xl text-black  border-black hover:border-gray-900 px-4 py-2 "
+          >
             add to cart
           </button>
         </div>

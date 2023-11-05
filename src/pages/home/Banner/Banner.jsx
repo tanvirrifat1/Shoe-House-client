c22e51e3b615aff889619117063e2800bd309e4b
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-11-05 23:15:46 +0600

    added more

--- src/pages/home/Banner/Banner.jsx
+++ src/pages/home/Banner/Banner.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import img6 from "../../../assets/assets/cover/client.png";
-import img from "../../../assets/assets/cover/bg.png";
+import img from "../../../assets/assets/cover/cover.png";
+import "./Banner.css";
+import HelpLine from "./HelpLine";
 
 const Banner = () => {
   const myStyle = {
@@ -11,23 +13,32 @@ const Banner = () => {
   };
 
   return (
-    <div style={myStyle} className="hero text-white">
-      <div className="hero-content text-center">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <img className="lg:h-[570px]" src={img6} alt="" />
-          <div className="text-start">
-            <div className="text-start my-3">
-              <h1 className="text-5xl font-bold">Ready</h1>
-              <h1 className="text-5xl font-bold">For Help</h1>
-              <h1 className="text-5xl font-bold">You</h1>
+    <div>
+      <div>
+        <div style={myStyle} className="hero text-white bg-blue-gray-900">
+          <div className="hero-content text-center ">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+              {/* <img className="lg:h-[568px] " src={img6} alt="" /> */}
+              <div className="text-start">
+                <div className="text-start ">
+                  <h1 className="text-5xl py-3 font-bold">Ready</h1>
+                  <h1 className="text-5xl py-3 font-bold">For Help</h1>
+                  <h1 className="text-5xl py-3 font-bold">You</h1>
+                </div>
+                <p className="py-6">
+                  We are certified company. We provide best plumbing services
+                  for you & your company .
+                </p>
+                <button className="btn bg-[#455a64] text-white hover:text-black">
+                  Book online
+                </button>
+              </div>
             </div>
-            <p className="py-6">
-              We are certified company. We provide best plumbing services for
-              you & your company .
-            </p>
-            <button className="btn btn-primary">Get Started</button>
           </div>
         </div>
+      </div>
+      <div>
+        <HelpLine />
       </div>
     </div>
   );
---
+++ src/pages/home/Banner/HelpLine.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { AiFillCar, AiOutlineFieldTime } from "react-icons/ai";
+import { FaPencilRuler } from "react-icons/fa";
+const HelpLine = () => {
+  return (
+    <section className="py-6 dark:bg-gray-800 -mt-36">
+      <div className="container flex flex-col justify-center p-4 mx-auto">
+        <div className="grid mx-auto gap-32 grid-cols-1  lg:grid-cols-3 sm:grid-cols-2">
+          <div className="card w-60  h-60 flex justify-center items-center  rounded-full shadow-xl">
+            <div className="flex-col px-2 flex justify-center items-center w">
+              <h2 className="text-8xl">
+                <AiOutlineFieldTime />
+              </h2>
+              <p className="font-semibold">Book Online</p>
+            </div>
+          </div>
+          <div className="card w-60 -mt-10 bg-[#455a64] text-white h-60 flex justify-center items-center  rounded-full shadow-xl">
+            <div className="flex-col px-2 flex justify-center items-center w">
+              <h2 className="text-8xl">
+                <AiFillCar />
+              </h2>
+              <p className="font-semibold">We Drive</p>
+            </div>
+          </div>
+          <div className="card w-60  h-60 flex justify-center items-center  rounded-full shadow-xl">
+            <div className="flex-col px-2 flex justify-center items-center w">
+              <h2 className="text-8xl">
+                <FaPencilRuler />
+              </h2>
+              <p className="font-semibold mt-1">Solve Problem</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HelpLine;
